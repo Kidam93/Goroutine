328a17e8053101d67d6f15274eb3cb3bb309c67a
benjamin.leyrat@pegase-market.com <benjamin.leyrat@pegase-market.com>
2021-06-19 13:02:21 +0200

    ex channel 1

--- channels/channel.go
+++ channels/channel.go
@@ -1,2 +1,27 @@
 package channel
 
+import (
+	"fmt"
+	"time"
+)
+
+
+func ping(c chan string) {
+	for i := 0; ; i++ {
+		c <- fmt.Sprintf("ping %v", i)
+	}
+}
+
+func print(c chan string) {
+	for {
+		msg := <- c
+		fmt.Println(msg)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func Run() {
+	c := make(chan string)
+	go ping(c)
+	print(c)
+}
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
+import (
+	channel "goroutine/channels"
+)
 
 func main() {
-	
+	channel.Run()
 }
